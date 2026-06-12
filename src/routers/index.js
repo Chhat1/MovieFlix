1cbe8e68de7079fff6f2265bcb0f4132e8f6ae1b
PHUK RICHHAT <richart718183@gmail.com>
2026-06-12 22:53:07 +0700

    Update

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,8 +3,7 @@ import Browse from "../components/pages/Browse.vue";
 import Home from "../components/pages/Home.vue";
 import MyList from "../components/pages/MyList.vue";
 import Details from "../components/pages/Details.vue";
-import Success from "../components/pages/success.vue";
-
+import Success from "../components/pages/Success.vue"
 
 const routes = [
     {
